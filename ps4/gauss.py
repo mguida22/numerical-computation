774902bdcd0992b0a0eefe7a75e3cde20a880239
mguida22 <michael.guida@colorado.edu>
2016-02-10 23:33:50 -0700

    mike never reads instructions

--- ps4/gauss.py
+++ ps4/gauss.py
@@ -1,13 +1,17 @@
+vector = [18, -3.0, 5.0]
+
 matrix = [
-    [-11.0, 3.0, 3.0, 18.0],
-    [-2.0, -5.0, 1.0, -3.0],
-    [1.0, -10.0, 8.0, 5.0]
+    [-11.0, 3.0, 3.0],
+    [-2.0, -5.0, 1.0],
+    [1.0, -10.0, 8.0]
 ]
 
-def print_matrix(m):
-    for row in m:
+def pprint(m, v):
+    for i, row in enumerate(m):
+        row.append(vector[i])
         print(row)
+        row.pop()
 
     print()
 
-print_matrix(matrix)
+pprint(matrix, vector)

46336d22800f071a66fbe9f2241eb6aec4bbe976
mguida22 <michael.guida@colorado.edu>
2016-02-11 13:29:53 -0700

    touch up

--- ps4/gauss.py
+++ ps4/gauss.py
@@ -1,11 +1,26 @@
-vector = [18, -3.0, 5.0]
-LEN = len(vector);
+# vector = [18, -3.0, 5.0]
+#
+# matrix = [
+#     [-11.0, 3.0, 3.0],
+#     [-2.0, -5.0, 1.0],
+#     [1.0, -10.0, 8.0]
+# ]
+# vector = [1.75, -0.21, 0.27]
+#
+# matrix = [
+#     [1.03, 5.46, 1.77],
+#     [-0.25, -1.5, 0.50],
+#     [0.16, 0.85, 0.34]
+# ]
+vector = [-5, -2, 1]
 
 matrix = [
-    [-11.0, 3.0, 3.0],
-    [-2.0, -5.0, 1.0],
-    [1.0, -10.0, 8.0]
+    [-2, 1, 4],
+    [7, -3, 1],
+    [3, -1, -7]
 ]
+
+LEN = len(vector);
 
 def pprint(m, v, x=None):
     for i, row in enumerate(m):
@@ -26,6 +41,10 @@ for i, row in enumerate(matrix):
     if (row[i] == 0):
         raise ZeroDivisionError('Zero along the diagonal.')
 
+print()
+print('Forward eliminate')
+print()
+
 # forward eliminate
 # current row, used for diagonal element
 for i in range(0, LEN-1):
@@ -42,24 +61,31 @@ for i in range(0, LEN-1):
         vector[j] = vector[j] - fraction * vector[i]
         pprint(matrix, vector)
 
+print()
+print('Back substitute')
+print()
+
 # back substitute
 # setup result array
 result = [0] * LEN
-# we know the last element so assign it
-result[LEN-1] = vector[LEN-1] / matrix[LEN-1][LEN-1]
 # iterate through the rest backwards to find remaining results
-for i in range(LEN-2, -1, -1):
+for i in range(LEN-1, -1, -1):
+    # calculate the result for each row using the matrix entries
     sums = vector[i]
     for j in range(i+1, LEN):
         sums = sums - matrix[i][j] * result[j]
     result[i] = sums / matrix[i][i]
     pprint(matrix, vector, result)
 
+# print out results nicely
+print()
 print('Final Result:')
 print()
 print('Of the form [A | b]:')
-for row in matrix:
+for i, row in enumerate(matrix):
+    row.append(vector[i])
     print(row)
+    row.pop()
 print()
 print('x:')
 print(result)

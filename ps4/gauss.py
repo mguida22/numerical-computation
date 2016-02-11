fb89903096d3559bf8f8111e4cf11e3ebb033cc6
mguida22 <michael.guida@colorado.edu>
2016-02-10 23:25:16 -0700

    init ps4

---
+++ ps4/gauss.py
@@ -0,0 +1,13 @@
+matrix = [
+    [-11.0, 3.0, 3.0, 18.0],
+    [-2.0, -5.0, 1.0, -3.0],
+    [1.0, -10.0, 8.0, 5.0]
+]
+
+def print_matrix(m):
+    for row in m:
+        print(row)
+
+    print()
+
+print_matrix(matrix)

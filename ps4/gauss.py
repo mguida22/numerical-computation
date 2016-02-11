a7487b8069c427fe59b18115c2d12249aed244db
mguida22 <michael.guida@colorado.edu>
2016-02-11 00:17:34 -0700

    works?

--- ps4/gauss.py
+++ ps4/gauss.py
@@ -2,13 +2,13 @@ vector = [18, -3.0, 5.0]
 
 matrix = [
     [-11.0, 3.0, 3.0],
-    [-2.0, -0, 1.0],
+    [-2.0, -5.0, 1.0],
     [1.0, -10.0, 8.0]
 ]
 
 def pprint(m, v):
     for i, row in enumerate(m):
-        row.append(vector[i])
+        row.append(v[i])
         print(row)
         row.pop()
 
@@ -19,8 +19,17 @@ pprint(matrix, vector)
 # Check if zeros on diagonal b/c that'll mean we need to divide by zero
 for i, row in enumerate(matrix):
     if (row[i] == 0):
-        raise ZeroDivisionError('Zero along the diagonal.'')
+        raise ZeroDivisionError('Zero along the diagonal.')
 
 # forward eliminate
+for row in range(0, len(vector)-1):
+    for i in range(row+1, len(vector)):
+        factor = matrix[i][row] / matrix[row][row]
+        for j in range(row, len(vector)):
+            matrix[i][j] = matrix[i][j] - factor * matrix[row][j]
 
-# back substitute
+        vector[i] = vector[i] - factor * vector[row]
+
+    pprint(matrix, vector)
+
+pprint(matrix, vector)

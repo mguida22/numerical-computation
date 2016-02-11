2d93048c5f922958a1393535c896209676b966b3
mguida22 <michael.guida@colorado.edu>
2016-02-10 23:43:17 -0700

    check zero on diag

--- ps4/gauss.py
+++ ps4/gauss.py
@@ -2,7 +2,7 @@ vector = [18, -3.0, 5.0]
 
 matrix = [
     [-11.0, 3.0, 3.0],
-    [-2.0, -5.0, 1.0],
+    [-2.0, -0, 1.0],
     [1.0, -10.0, 8.0]
 ]
 
@@ -15,3 +15,12 @@ def pprint(m, v):
     print()
 
 pprint(matrix, vector)
+
+# Check if zeros on diagonal b/c that'll mean we need to divide by zero
+for i, row in enumerate(matrix):
+    if (row[i] == 0):
+        raise ZeroDivisionError('Zero along the diagonal.'')
+
+# forward eliminate
+
+# back substitute

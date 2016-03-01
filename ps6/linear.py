7a43e8ca4bdf06a6f1161eadbb7d175b0d6f5021
mguida22 <michael.guida@colorado.edu>
2016-02-29 17:31:53 -0700

    add lagrange interpolation

--- ps6/linear.py
+++ ps6/linear.py
@@ -47,3 +47,14 @@ print(y)
 # interpolate using (1790,1990) for 2050
 y = interpolate(population[0], population[-1], 2050)
 print(y)
+
+def lagrange(a, b, c, x):
+    # lagrange equation from textbook
+    t1 = a[1] * (((x - b[0]) * (x - c[0])) / ((a[0] - b[0]) * (a[0] - c[0])))
+    t2 = b[1] * (((x - a[0]) * (x - c[0])) / ((b[0] - a[0]) * (b[0] - c[0])))
+    t3 = c[1] * (((x - a[0]) * (x - b[0])) / ((c[0] - a[0]) * (c[0] - b[0])))
+
+    return (t1 + t2 + t3)
+
+y = lagrange(population[-3], population[-2], population[-1], 1985)
+print(y)

da67aa8f4560ec72a4fd964d441fda1535e648d4
mguida22 <michael.guida@colorado.edu>
2016-03-08 12:57:01 -0700

    got the equations

--- ps7/cubic-spline.py
+++ ps7/cubic-spline.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import zeros
 
-x = [1, 2, 3, 4, 5, 6, 7]
-y = [1, 3, 2, 1, 2, 4, 5]
+x = [1., 2., 3., 4., 5., 6., 7.]
+y = [1., 3., 2., 1., 2., 4., 5.]
 n = len(x)
 
 a = zeros(n)
@@ -30,23 +30,28 @@ for i in range(1, n-1):
 	A[i][i] = 2 * (dx[i] + dx[i+1])
 	A[i][i+1] = dx[i+1]
 	r[i] = 3 * ((dy[i+1] / dx[i+1]) - (dy[i] / dx[i]))
+	if np.isnan(r[i]):
+		r[i] = 0
 
 print(A)
 print(r)
 
+c = np.linalg.solve(A, r)
+print(c)
+
+d = zeros(n)
+b = zeros(n)
+for i in range(0, n-1):
+    d[i] = (c[i+1] - c[i]) / (3 * dx[i])
+    b[i] = (dy[i] / dx[i]) - ((dx[i] / 3) * (2 * c[i] + c[i+1]))
+
+for i in range(0, n-1):
+    print('S{i}(x) = {a} + {b}(x - {x}) + {c}(x - {x})^2 + {d}(x - {x})^3'.format(i=i, a=y[i], b=b[i], c=c[i][0], d=d[i], x=x[i]))
 
 
-# for i in range(0, n-1):
-#     d[i] = (c[i+1] - c[i]) / (3 * dx[i])
-#     b[i] = (dy[i] / dx[i]) - ((dx[i] / 3) * (2 * c[i] + c[i+1]))
-
-# for i in range(0, n-1):
-#     print('S{i}(x) = {a} + {b}(x - {x}) + {c}(x - {x})^2 + {d}(x - {x})^3').format(i, a=y[i], b=b[i], c=c[i], d=d[i], x=x[i])
-
-
-# plt.plot(x, y)
-# plt.title('Cubic Splines',fontsize=10)
-# plt.show()
+plt.plot(x, y)
+plt.title('Cubic Splines',fontsize=10)
+plt.show()
 
 
 
